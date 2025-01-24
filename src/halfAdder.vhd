68eec8ca60803c4224025fb6de78227cd0cc5a61
mattgromines <mattgromines@gmail.com>
2025-01-23 22:01:20 -0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   halfAdder.runs/impl_1/halfAdder.bit 	modified:   halfAdder.xpr 	modified:   src/Basys3_Master.xdc 	modified:   src/halfAdder.vhd 	modified:   src/halfAdder_tb.v

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -29,7 +29,8 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_S     : out std_logic;
+	o_Cout  : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
 	-- TODO:  Carry port
   ); -- the semicolon is here instead
 end halfAdder;
@@ -42,6 +43,7 @@ begin
 
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
+	o_Cout <= i_A and i_B;
 	-- TODO:  Carry signal assignment
 	
 end halfAdder_arch;
